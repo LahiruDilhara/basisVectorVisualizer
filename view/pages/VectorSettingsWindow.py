a87880d44acce509bb15dd9bd0133c1e28b942a0
galahirudilhara <galahirudilhara@gmail.com>
2025-04-06 23:46:05 +0530

    add the vector settings window add buttonc

--- view/pages/VectorSettingsWindow.py
+++ view/pages/VectorSettingsWindow.py
@@ -3,6 +3,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont, QColor
 
 from ..components import NewVectorPanel
+from ..widgets import sidePanelButton
 
 
 class VectorSettingsWindow(QWidget):
@@ -16,5 +17,10 @@ class VectorSettingsWindow(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # Create button
+        addButton = sidePanelButton.SidePanelButton(
+            "Add Vector", buttonColor="#4CAF50", buttonHoverColor="#45a049", buttonPressedColor="#3e8e41", onPressed=None)
+
         layout.addWidget(NewVectorPanel.NewVectorPanel(
             vectorPanelSpec=self.vectorPanelSpec))
+        layout.addWidget(addButton)
